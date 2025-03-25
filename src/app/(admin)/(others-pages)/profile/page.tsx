9ed0223541aebcfdc79fe0923ad63e558a6f8882
Lucca <claudia@coffee-code.tech>
2025-03-25 16:28:22 -0300

    remove metadata

--- src/app/(admin)/(others-pages)/profile/page.tsx
+++ src/app/(admin)/(others-pages)/profile/page.tsx
@@ -1,11 +1,4 @@
 import UserMetaCard from '@/components/user-profile/UserMetaCard'
-import { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'Next.js Profile | TailAdmin - Next.js Dashboard Template',
-  description:
-    'This is Next.js Profile page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template',
-}
 
 export default function Profile() {
   return (
